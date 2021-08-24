2076ab588b874d1737328ad3c785cc90033e9c33
Pavel Potemkin <specialforbtsu@gmail.com>
2021-08-24 21:49:52 +0300

    cache

---
+++ projects/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import Group, Permission, models
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.CharFiled(max_length=30, unique=True)
+
+
