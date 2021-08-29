9afbc542ee32d453e0cd5e7a8f3a98a84853db10
Pavel Potemkin <specialforbtsu@gmail.com>
2021-08-29 17:02:42 +0300

    minor changes

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import Group, Permission, models
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=30)
-    owner = models.CharField(max_length=30, unique=True)
+    project_name = models.CharField(max_length=30, unique=True)
+    owner = models.ForeignKey('Owner', related_name='Project')
+    bio = models.CharField(max_length=1500)
+
+    current_amount = models.IntegerField(default=1)
+    max_amount = models.IntegerField(default=4)
+
+
+
 
 
